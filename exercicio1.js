7eabd8c19022ab8a77b0cd36ea35393e98354df2
Gabrieli <gabrielidelimamartins@gmail.com>
2026-05-10 15:14:22 -0300

    exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,3 +1,13 @@
+/*
+Acumulando o valor de vendas: Usar o laço for para calcular o total de vendas de uma
+loja, dado um array com os valores das transações.
+
+Instruções:
+● Criar um array com valores de vendas diárias (por exemplo: [100, 200, 150, 300]);
+● Usar o laço for para somar todos os valores;
+● No final, exibir o valor total acumulado de vendas no console.
+ */
+
 let vendasDiarias = [100, 120, 300, 200, 130, 600, 450, 110];
 let vendasTotais = 0
 
