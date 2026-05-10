a75b60d489e27fbd132d395e80943a4a89f3f8d4
Gabrieli <gabrielidelimamartins@gmail.com>
2026-05-10 15:08:13 -0300

    exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,4 @@
-let vendasDiarias = [100, 120, 300, 200, 150, 130, 600, 450, 110, 90, 115];
+let vendasDiarias = [100, 120, 300, 200, 150, 130, 600, 450, 110, 90];
 let vendasTotais = 0
 
 for (let i = 0; i < 10; i++){
