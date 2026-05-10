c9105ac1ec6b0253259f89317dba4cc6bf0da795
Gabrieli <gabrielidelimamartins@gmail.com>
2026-05-10 15:10:28 -0300

    exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,7 +1,7 @@
-let vendasDiarias = [100, 120, 300, 200, 150, 130, 600, 450, 110, 90];
+let vendasDiarias = [100, 120, 300, 200, 130, 600, 450, 110];
 let vendasTotais = 0
 
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < vendasDiarias.length; i++){
   vendasTotais = vendasDiarias[i] + vendasTotais;
 }
 
